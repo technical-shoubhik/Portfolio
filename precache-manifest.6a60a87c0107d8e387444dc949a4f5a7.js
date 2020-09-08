82d6ccbc6e73dba86d3788fd150d540adc6e9da8
Shoubhik Raj <64132039+technical-shoubhik@users.noreply.github.com>
2020-09-08 23:34:47 +0530

    added more files

---
+++ precache-manifest.6a60a87c0107d8e387444dc949a4f5a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a905b02384e1eece21a66f4322395e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6f6db30fed0afd1e369",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6f6db30fed0afd1e369",
+    "url": "/static/js/main.1c0e6b00.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
